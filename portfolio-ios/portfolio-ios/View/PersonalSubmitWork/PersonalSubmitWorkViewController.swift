135e3a7b124ff78b4e957187ad56fc5610ab0cf8
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-25 21:16:46 +0900

    [add]ファイル追加

---
+++ portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PersonalSubmitWorkViewController.swift
+//  portfolio-ios
+//
+//  Created by KaitoKudo on 2021/10/25.
+//
+
+import UIKit
+
+class PersonalSubmitWorkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
