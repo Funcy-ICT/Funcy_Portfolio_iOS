cfd5f36ab7cbb4b6f9a0a58c6bd297773eb7cb55
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-25 17:02:16 +0900

    [add]MainTabBarを設定

---
+++ portfolio-ios/portfolio-ios/View/PersonalMyPage/PersonalMyPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PersonalMyPageViewController.swift
+//  portfolio-ios
+//
+//  Created by KaitoKudo on 2021/10/25.
+//
+
+import UIKit
+
+class PersonalMyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
