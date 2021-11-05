855301a1ec09e525a2220507dcbcb91a02273707
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-11-06 02:52:32 +0900

    [add]iOS13以上の時に画像を灰色で表示

--- portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
+++ portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
@@ -9,7 +9,23 @@ import UIKit
 
 class PersonalSubmitWorkViewController: UIViewController {
 
+    @IBOutlet weak var addTagButton: UIButton!
+    @IBOutlet weak var addThumbnailsWorkButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if #available(iOS 13.0, *) {
+            addTagButton.tintColor = .gray
+            addThumbnailsWorkButton.tintColor = .gray
+        }
+    }
+
+    @IBAction func addTag(_ sender: Any) {
+        print(#function)
+    }
+
+    @IBAction func addThumbnailsWork(_ sender: Any) {
+        print(#function)
     }
 }
