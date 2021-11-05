386955dccf046d4ab28ba81272f0b0bd2100f40f
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-11-06 03:09:01 +0900

    [add]UITextViewに枠線を追加

--- portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
+++ portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
@@ -11,14 +11,22 @@ class PersonalSubmitWorkViewController: UIViewController {
 
     @IBOutlet weak var addTagButton: UIButton!
     @IBOutlet weak var addThumbnailsWorkButton: UIButton!
+    @IBOutlet weak var workDescriptionTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configurationUI()
+    }
 
+    private func configurationUI() {
         if #available(iOS 13.0, *) {
             addTagButton.tintColor = .gray
             addThumbnailsWorkButton.tintColor = .gray
         }
+        workDescriptionTextView.layer.borderColor = UIColor.gray.cgColor
+        workDescriptionTextView.layer.borderWidth = 0.2
+        workDescriptionTextView.layer.cornerRadius = 5.0
+        workDescriptionTextView.layer.masksToBounds = true
     }
 
     @IBAction func addTag(_ sender: Any) {
