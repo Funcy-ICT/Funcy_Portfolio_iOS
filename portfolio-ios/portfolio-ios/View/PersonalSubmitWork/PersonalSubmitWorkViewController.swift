b087697381c8fe0367d536216aa16b78db7b3b35
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-11-08 18:25:34 +0900

    [add]カメラロールから写真を選択可能にした

--- portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
+++ portfolio-ios/portfolio-ios/View/PersonalSubmitWork/PersonalSubmitWorkViewController.swift
@@ -5,9 +5,10 @@
 //  Created by KaitoKudo on 2021/10/25.
 //
 
+import PhotosUI
 import UIKit
 
-class PersonalSubmitWorkViewController: UIViewController {
+class PersonalSubmitWorkViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var addTagButton: UIButton!
     @IBOutlet weak var addThumbnailsWorkButton: UIButton!
@@ -87,6 +88,47 @@ class PersonalSubmitWorkViewController: UIViewController {
 
     @IBAction func addThumbnailsWork(_ sender: Any) {
         print(#function)
+        
+        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
+
+        // カメラが利用可能かどうかチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            // カメラを起動するための選択肢を定義
+            // UIAlertActionは選択肢を表示させるクラス
+            let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: { (_: UIAlertAction) in
+
+                // カメラを起動
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .camera
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
+        }
+
+        // フォトライブラリが利用可能かどうかチェック
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            // フォトライブラリを起動するための選択肢を定義
+            let photoLibraryAction = UIAlertAction(title: "フォトライブラリ", style: .default, handler: { (_: UIAlertAction) in
+
+                // フォトライブラリを起動
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .photoLibrary
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+
+        // キャンセルの選択肢を定義
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+
+        // ipadで落ちてしまう対策
+        alertController.popoverPresentationController?.sourceView = view
+
+        // 選択肢を画面に表示
+        present(alertController, animated: true, completion: nil)
     }
 
     @IBAction func submitWork(_ sender: Any) {
