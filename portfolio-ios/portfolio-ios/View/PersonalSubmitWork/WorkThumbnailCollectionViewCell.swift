e2e36d4cca07ab55d88f0a957ba41826776cae0b
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-11-29 15:17:38 +0900

    [add]作品サムネイルカスタムセルを作成

---
+++ portfolio-ios/portfolio-ios/View/PersonalSubmitWork/WorkThumbnailCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  WorkThumbnailCollectionViewCell.swift
+//  portfolio-ios
+//
+//  Created by KaitoKudo on 2021/11/29.
+//
+
+import UIKit
+
+class WorkThumbnailCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var workThumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBAction func deleteWorkThumbnail(_ sender: Any) {
+    }
+
+}
