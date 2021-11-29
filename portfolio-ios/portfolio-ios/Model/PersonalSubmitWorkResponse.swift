2143d93ddbbcbea8a1c528e9a45cf54c24ce67e8
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-11-29 18:26:00 +0900

    [add]モデル作成

---
+++ portfolio-ios/portfolio-ios/Model/PersonalSubmitWorkResponse.swift
@@ -0,0 +1,12 @@
+//
+//  PersonalSubmitWorkResponse.swift
+//  portfolio-ios
+//
+//  Created by KaitoKudo on 2021/11/29.
+//
+
+import Foundation
+
+struct PersonalSubmitWorkResponse: Codable {
+    let articleID: String
+}
