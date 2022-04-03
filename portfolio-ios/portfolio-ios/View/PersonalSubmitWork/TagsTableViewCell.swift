16288069812026cd9baa31686296cfb54fcfe63c
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-04-03 18:21:50 +0900

    [add]タグを表すカスタムセルを作成

---
+++ portfolio-ios/portfolio-ios/View/PersonalSubmitWork/TagsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TagsTableViewCell.swift
+//  portfolio-ios
+//
+//  Created by 工藤 海斗 on 2022/04/03.
+//
+
+import UIKit
+
+class TagsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tagLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
